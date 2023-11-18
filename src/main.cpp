8734814cd06284de251f4ba75ab721c818a667d8
Noah Gitsham <nhgitsham@gmail.com>
2023-11-18 05:32:25 +0000

    Add glad and GLFW dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 
-int main() {
-	std::cout <<  "Hello, World!" << std::endl;
+#include<glad/gl.h>
+#include<GLFW/glfw3.h>
+
+int main (int argc, char *argv[]) {
+	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
